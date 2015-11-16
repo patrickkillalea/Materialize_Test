f7c1e48b9ef66c81c611126b118f8123f24987f0
patrickkillalea <killalea.patrick@gmail.com>
2015-11-16 12:09:30 +0000

    Added form section

--- js/materialize.js
+++ js/materialize.js
@@ -6475,4 +6475,13 @@ settings.klass.table,
   }
   );
 
+$(document).ready(function() {
+    $('select').material_select();
+  });
+
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+
 }( jQuery ));
